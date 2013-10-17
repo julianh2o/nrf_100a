ed990da86d6b87dc5eee5ad2ec3e49bf1c53d676
Julian Hartline <julianh2o@gmail.com>
2013-10-17 11:42:33 -0700

    initial commit

---
+++ build/default/production/serlcd.o.d
@@ -0,0 +1,5 @@
+build/default/production/serlcd.o:  \
+	/Users/julian/Documents/Programming/tinkering/tinkering_github/pic18f25k80.X/serlcd.c  \
+	/Users/julian/Documents/Programming/tinkering/tinkering_github/pic18f25k80.X/serlcd.h  \
+	/Users/julian/Documents/Programming/tinkering/tinkering_github/pic18f25k80.X/serlcd_config.h  \
+
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=nrf_100a.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/nrf_100a.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=nrf100a.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/nrf100a.tar
